bb41faf56171c4942c3a19a93ce638d181a76a37
GustavoBodi <gustavorbodi@gmail.com>
2024-10-23 10:23:02 -0300

    Added test for adicionario_funcionario_roberto_no_banco

--- tests/EmpresaTest.py
+++ tests/EmpresaTest.py
@@ -1,4 +1,5 @@
 import unittest
+from src.Funcionario import Funcionario
 from src.Empresa import Empresa
 
 
@@ -14,3 +15,15 @@ class EmpresaTest(unittest.TestCase):
         nome_empresa = ""
         with self.assertRaises(Exception):
             empresa = Empresa(nome_empresa)
+
+    def teste_adicionar_funcionario_roberto_no_banco(self):
+        nome_empresa = "Banco"
+        empresa = Empresa(nome_empresa)
+        funcionario = Funcionario("roberto", "cpf", 1000, "chefe")
+
+        empresa.adicionar_funcionario(funcionario)
+
+        assert empresa.funcionarios[0].nome == "roberto"
+        assert empresa.funcionarios[0].cpf == "cpf"
+        assert empresa.funcionarios[0].salário == 1000
+        assert empresa.funcionarios[0].cargo == "chefe"

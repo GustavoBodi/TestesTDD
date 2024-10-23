c3fc4b0a9cb68068f2e5dde0d53cbe2bf229272a
GustavoBodi <gustavorbodi@gmail.com>
2024-10-23 09:59:02 -0300

    Added test for Empresa with empty name

--- tests/EmpresaTest.py
+++ tests/EmpresaTest.py
@@ -9,3 +9,8 @@ class EmpresaTest(unittest.TestCase):
         assert empresa.funcionarios == []
         assert empresa.projetos == []
         assert empresa.nome == nome_empresa
+
+    def teste_criar_empresa_nome_vazio(self):
+        nome_empresa = ""
+        with self.assertRaises(Exception):
+            empresa = Empresa(nome_empresa)
